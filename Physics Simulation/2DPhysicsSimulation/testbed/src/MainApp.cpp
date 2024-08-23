a1328f5f2b6ab4342724c91a99f7da1f77852f4f
Caleb Derrickson <Calebderrickson@gmail.com>
2024-08-23 00:46:49 -0500

    Shaders. Added stb_image. Drew Square. Tons of refactoring to do.

--- Physics Simulation/2DPhysicsSimulation/testbed/src/MainApp.cpp
+++ Physics Simulation/2DPhysicsSimulation/testbed/src/MainApp.cpp
@@ -4,7 +4,22 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+#if 0
+    // Here for the sake of remembering I have this.
+    #define STB_IMAGE_IMPLEMENTATION
+    #include <stb_image.h>
+#endif
 // # TODO: End Temporary
+
+// To compile shader/fragment programs
+// TODO: Refactor this
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+#include <string>
+
+
 
 // *******************************
 //    PRIVATE METHOD DECLARATIONS 
@@ -13,6 +28,9 @@
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+// To compile shaders
+// TODO: Refactor
+std::string loadShaderSrc(const char* filename);
 
 // *******************************
 //    CLASS METHOD DEFINITIONS
@@ -39,21 +57,110 @@ STATE MainApp::init()
         std::cout<<"Error in initialization"<<std::endl;
         return STATE::ERROR;
     }
+
+    const GLubyte* version = glGetString(GL_VERSION);
+    std::cout<<"Using OpenGL version "<< version<<std::endl;
+
     glViewport(0, 0, _width, _height);
 
     // Setting window update on resize
     glfwSetFramebufferSizeCallback(_mainWindow, framebufferSizeCallback);
+
+    // Begin Shader Compile
+    // One shaderprogram per different colored "thing"
+    // For the sake of refactoring, make an array of shaderprograms,
+    // maybe a hashtable from "thing" to shaderProgram
+    // For each different colored thing, need to modify the offset of the draw command in the main loop
+
+    // TODO: Refactor
+    int success;
+    char infoLog[512];
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    std::string vertexShadersrc = loadShaderSrc("../testbed/assets/vertex_core.glsl");
+    const GLchar* vertShader = vertexShadersrc.c_str();
+    glShaderSource(vertexShader, 1, &vertShader, NULL);
+    glCompileShader(vertexShader);
+
+    // catch error
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout<<"Error in vertex shader compilation: "<<std::endl<<infoLog<<std::endl;
+    }
+
+
+    // Begin Fragment Compile
+    // TODO: Refactor
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    std::string fragmentShadersrc = loadShaderSrc("../testbed/assets/fragment_core.glsl");
+    const GLchar* fragShader = fragmentShadersrc.c_str();
+    glShaderSource(fragmentShader, 1, &fragShader, NULL);
+    glCompileShader(fragmentShader);
+
+    // catch error
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout<<"Error in fragment shader compilation: "<<std::endl<<infoLog<<std::endl;
+    }
+
+    // Creating shader programs for linking
+    _shaderProgram = glCreateProgram();
+
+    glAttachShader(_shaderProgram, vertexShader);
+    glAttachShader(_shaderProgram, fragmentShader);
+    glLinkProgram(_shaderProgram);
+
+    // catch error
+    glGetProgramiv(_shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(_shaderProgram, 512, NULL, infoLog);
+        std::cout<< "Linking Error:" <<std::endl<<infoLog<<std::endl;
+    }
+
+    // can delete vertex and frag shader now
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    // # TODO: End Shaders
+
+    // VAO, VBO start
+    // TODO: Refactor
+
+    // vertex 
+    float vertices[] = {
+        // first trangle
+        0.5f, 0.5f, 0.0f,   // top right
+        -0.5f, 0.5f, 0.0f,  // top left
+        -0.5f, -0.5f, 0.0f, // bottom left
+        0.5f, -0.5f, 0.0f   // bottom right
+    };
+
+    unsigned int indices[] = {
+      0, 1, 2, // First Triangle 
+      2, 3, 0  // Second Triange
+    };
     
-    // # TODO: Temporary test of GLM
+    glGenVertexArrays(1, &_VAO);
+    glGenBuffers(1, &_VBO);
+    glGenBuffers(1, &_EBO);
 
-    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
-    vec = trans * vec;
-    std::cout<<vec.x<<' '<<vec.y<<' '<<vec.z<<std::endl;
+    // bind vao
+    glBindVertexArray(_VAO);
 
+    // bind vbo
+    glBindBuffer(GL_ARRAY_BUFFER, _VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // # TODO: End temporary
+    // set attribute pointer 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3* sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // bind ebo
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // TODO: Done VAO, VBO, EBO
 
 
     return STATE::OKAY;
@@ -66,6 +173,11 @@ STATE MainApp::run()
         processInput(_mainWindow);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw shapes
+        glBindVertexArray(_VAO);
+        glUseProgram(_shaderProgram);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(_mainWindow);
         glfwPollEvents();
@@ -98,4 +210,25 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     } 
+}
+
+std::string loadShaderSrc(const char* filename)
+{
+    std::ifstream file;
+    std::stringstream buf;
+
+    std::string ret = "";
+
+    file.open(filename);
+
+    if (!file.is_open()) {
+        std::cerr<<"File "<<filename<<"is not open!"<<std::endl;
+        exit(1);
+    }
+
+    buf << file.rdbuf();
+    ret = buf.str();
+
+    file.close(); 
+    return ret;
 }
--- Physics Simulation/2DPhysicsSimulation/testbed/src/MainApp.hpp
+++ Physics Simulation/2DPhysicsSimulation/testbed/src/MainApp.hpp
@@ -14,4 +14,10 @@ public:
 private:
     // window pointer is provided by the BaseApp class
 
+    // TODO: refactor this
+    unsigned int _VAO;
+    unsigned int _VBO;
+    unsigned int _EBO;
+    unsigned int _shaderProgram;
+
 };
